c5c69caa917945ceb30b48e67b3940869f4c5d9f
Yuncheng Li (yli) <raingomm@gmail.com>
2014-12-21 14:00:53 -0500

    finished verify size

--- verify_size.sh
+++ verify_size.sh
@@ -4,5 +4,7 @@
 
 base_dir=`pwd`/datasets/ILSVRC2014
 
-find $base_dir -name '*.JPEG' | xargs identify > real-size.txt
-find $base_dir -name '*.xml'
+find $base_dir -name '*.JPEG' | xargs identify > $base_dir/real-size.txt
+find $base_dir -name '*.xml' | python parse_xml.py > $base_dir/xml-size.txt
+
+echo $0[`hostname -s`]: please run second half | mail -s "done" yli
