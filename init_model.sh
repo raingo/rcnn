8221fe3465242a157b32c0cba24a10840f79ca7f
Yuncheng Li <raingomm@gmail.com>
2015-01-08 13:42:56 -0500

    config caffe into the init_model.sh

--- init_model.sh
+++ init_model.sh
@@ -4,3 +4,5 @@
 model_dir=./data/rcnn_models/voc_exp-baseline_train/
 ln -s /homes/ycli/rcnn_model2.mat $model_dir/
 ln -s /homes/ycli/finetune_exp-baseline_train-iter-80K_iter_80000.caffemodel $model_dir
+
+ln -s /homes/ycli/caffe/ external/
