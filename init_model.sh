d4fa014c92c7e4eb27b871869f8a60ebdb5fb698
Yuncheng Li <ycli@yahoo-inc.com>
2015-01-08 11:15:52 -0800

    clean up and create folders

--- init_model.sh
+++ init_model.sh
@@ -2,7 +2,10 @@
 # vim ft=sh
 
 model_dir=./data/rcnn_models/voc_exp-baseline_train/
-ln -s /homes/ycli/rcnn_model2.mat $model_dir/
+mkdir -p $model_dir
+rm -f $model_dir/*
+ln -s /homes/ycli/rcnn_model2.mat $model_dir/rcnn_model.mat
 ln -s /homes/ycli/finetune_exp-baseline_train-iter-80K_iter_80000.caffemodel $model_dir
 
+rm -f external/caffe
 ln -s /homes/ycli/caffe/ external/
